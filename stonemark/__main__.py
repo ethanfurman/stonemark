6fb0ab23ac69f19e0f176706d94b2ead57a909c6
Ethan Furman <ethan@stoneleaf.us>
2022-04-28 14:54:23 -0700

    update __main__ for title and size options

--- stonemark/__main__.py
+++ stonemark/__main__.py
@@ -7,8 +7,11 @@ from . import Document
 @Command(
         source=Spec('name of source file to convert', type=Path),
         target=Spec('name of output file [default: source base name with .html extension]', default='', type=Path),
+	header_sizes=Spec('sizes for the three header categories', MULTI, abbrev='sizes', force_default=(1,2,3)),
+	header_title=Spec('make first header a title', FLAG, abbrev='title'),
+
         )
-def stonemark(source, target):
+def stonemark(source, target, header_sizes, header_title):
     if not source.exists():
         abort("'%s' does not exist" % source)
     if target == '':
@@ -17,7 +20,7 @@ def stonemark(source, target):
         target += source.filename.strip_ext() + '.html'
     with open(source) as f:
         text = f.read()
-    doc = Document(text).to_html()
+    doc = Document(text, header_sizes=header_sizes, first_header_is_title=header_title).to_html()
     with open(target, 'wb') as f:
         f.write(doc)
 
