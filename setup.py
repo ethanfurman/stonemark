dc2cd6b8b222a035c6516ef59a41bb54a794c77f
Ethan Furman <ethan@stoneleaf.us>
2022-07-09 00:39:28 -0700

    prep for version 0.2

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ make = []
 
 data = dict(
        name='stonemark',
-       version='0.1.10a1',
+       version='0.2.0a1',
        license='BSD License',
        description='a markup language similar to markdown',
        long_description=long_desc,
