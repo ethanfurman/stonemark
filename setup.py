f5d75169d85db8a13ea82c529a88754049cd04d8
Ethan Furman <ethan@stoneleaf.us>
2022-09-30 17:25:06 -0700

    version 0.2.5

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ make = []
 
 data = dict(
        name='stonemark',
-       version='0.2.5a3',
+       version='0.2.5',
        license='BSD License',
        description='a markup language similar to markdown',
        long_description=long_desc,
--- stonemark/__init__.py
+++ stonemark/__init__.py
@@ -100,7 +100,7 @@ __all__ = [
         'Document',
         ]
 
-version = 0, 2, 5, 3
+version = 0, 2, 5
 
     # HEADING = PARAGRAPH = TEXT = QUOTE = O_LIST = U_LIST = LISTITEM = CODEBLOCK = RULE = IMAGE = FOOTNOTE = LINK = ID = DEFINITION = None
     # END = SAME = CHILD = CONCLUDE = ORDERED = UNORDERED = None
