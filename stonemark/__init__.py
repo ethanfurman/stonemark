59af714b2126e564da49397f9fbba98b35bd5eef
Ethan Furman <ethan@stoneleaf.us>
2023-03-09 19:52:36 -0800

    version 0.3.3

--- stonemark/__init__.py
+++ stonemark/__init__.py
@@ -107,7 +107,7 @@ __all__ = [
         'Document',
         ]
 
-version = 0, 3, 3, 1
+version = 0, 3, 3
 
 HEADING = PARAGRAPH = TEXT = QUOTE = O_LIST = U_LIST = LISTITEM = CODEBLOCK = RULE = IMAGE = FOOTNOTE = LINK = ID = DEFINITION = TABLE = DETAIL = None
 END = SAME = CHILD = CONCLUDE = ORDERED = UNORDERED = None
